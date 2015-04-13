a0b18da1d31b3b0fe7e4b12c69a6966ad761845f
Kevin Weeks <kevinmichaelweeks@gmail.com>
2015-04-12 18:13:48 -0700

    Updated app list in installapps.sh

--- installapps.sh
+++ installapps.sh
@@ -1,15 +1,35 @@
-brew cask install filezilla
-brew cask install iterm2
-brew cask install google-chrome
-brew cask install sourcetree
-brew cask install sublime-text
+# Personal
 brew cask install evernote
+brew cask install spotify
 brew cask install toggldesktop
 brew cask install 1password
-brew cask install skype
-brew cask install spectacle
 brew cask install dropbox
-brew cask install flux
+brew cask install vlc
+brew cask install camtasia
+
+# Browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+
+# Dev
+brew cask install sourcetree
+brew cask install sublime-text
+brew cask install webstorm
+brew cask install filezilla
+brew cask install iterm2
+brew cask install robomongo
+
+# Communication
+brew cask install skype
+brew cask install slack
+
+# Utilities
 brew cask install alfred
 brew cask install appcleaner
-brew cask install webstorm
+brew cask install flux
+brew cask install spectacle
+brew cask install tunnelbear
+brew cask install bartender
+brew cask install caffeine
+brew cask install doxie
